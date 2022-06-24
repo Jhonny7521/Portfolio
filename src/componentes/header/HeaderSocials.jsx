85c8720730f53f47b01bae8c7c605f8f837594a4
Jhonny Quiliche <jhonny.jql1210@gmail.com>
2022-06-24 14:29:00 -0500

    segundo commit

--- src/componentes/header/HeaderSocials.jsx
+++ src/componentes/header/HeaderSocials.jsx
@@ -2,7 +2,6 @@ import React from 'react';
 import {BsLinkedin} from 'react-icons/bs';
 import {FaGithub} from 'react-icons/fa';
 import {FaTwitterSquare} from 'react-icons/fa';
-import { BiLogIn } from 'react-icons/bi';
 
 const HeaderSocials = () => {
   return (
@@ -10,7 +9,6 @@ const HeaderSocials = () => {
       <a href='https://www.linkedin.com/in/jhonnyquilichellaxa/' target='_blank' rel="noreferrer"><BsLinkedin/></a>
       <a href='https://github.com/Jhonny7521' target='_blank' rel="noreferrer"><FaGithub/></a>
       <a href='https://twitter.com/JhonnyQuiliche' target='_blank' rel="noreferrer"><FaTwitterSquare/></a>
-      <a href='/login' ><BiLogIn/></a>
     </div>
   )
 }
