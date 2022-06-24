3722877cffc90bd8192770b9ad098c895441ae66
Jhonny Quiliche <jhonny.jql1210@gmail.com>
2022-06-24 14:58:46 -0500

    cuarto commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,37 +1,26 @@
 import React from 'react';
-// import Header from './componentes/header/Header';
-// import Nav from './componentes/nav/Nav';
-// import Experiencia from './componentes/experiencia/Experiencia';
-// import Servicios from './componentes/servicios/Servicios';
-// import Testimonios from './componentes/testimonios/Testimonios';
-// import Contacto from './componentes/contacto/Contacto'
-// import Footer from './componentes/footer/Footer'
-// import Portfolio from './componentes/portfolio/Portfolio';
-// import About from './componentes/about/About';
-import Login from './componentes/login/Login';
-import { BrowserRouter } from 'react-router-dom';
-import { Switch } from 'react-router-dom';
-import { Route } from 'react-router-dom';
-import Home from './componentes/home/Home';
+import Header from './componentes/header/Header';
+import Nav from './componentes/nav/Nav';
+import Experiencia from './componentes/experiencia/Experiencia';
+//import Servicios from './componentes/servicios/Servicios';
+//import Testimonios from './componentes/testimonios/Testimonios';
+import Contacto from './componentes/contacto/Contacto'
+import Footer from './componentes/footer/Footer'
+//import Portfolio from './componentes/portfolio/Portfolio';
+import About from './componentes/about/About';
 
 const App = () => {
   return (
     <>
-      {/* <Header />
+      <Header />
       <Nav />
       <About/>
-      <Experiencia /> */}
+      <Experiencia />
       {/* <Servicios /> */}
       {/* <Portfolio /> */}
       {/* <Testimonios /> */}
-      {/* <Contacto />
-      <Footer /> */}
-      <BrowserRouter>
-        <Switch>
-          <Route path='/' exact render={ () => <Home />}/>
-          <Route path='/login' exact render={ () => <Login />}/>
-        </Switch>
-      </BrowserRouter>
+      <Contacto />
+      <Footer /> 
     </>
   )
 }
